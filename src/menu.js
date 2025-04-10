e766b42f56b89166034fa90939bcce707fbb51c0
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-04-11 00:42:44 +0200

    Create menu.js

---
+++ src/menu.js
@@ -0,0 +1,11 @@
+export const menuSection = document.createElement("div");
+
+menuSection.innerHTML = `<div className="hero">
+<h1>This is the Menu</h1>
+<ul>
+<li>Burger</li>
+<li>Pizza</li>
+<li>Cool Drink</li>
+<li>Ice Cream</li>
+</ul>
+</div>`;
