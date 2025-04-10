c2a324ab242de22ae5c33262cfccddd5efa05e3b
Nkateko Shikuhele <shikuhelenkateko@gmail.com>
2025-04-11 00:42:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 // index.js
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import { menuSection } from "./menu";
+import { aboutSection } from "./about.js";
 
-console.log(greeting);
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const aboutBtn = document.querySelector("#about-btn");
+
+const content = document.querySelector("#content");
+
+const heroSection = document.createElement("div");
+
+heroSection.innerHTML = `<div className="hero">
+<h1>This is the Hero section</h1>
+<p>This is the page  users will see when they open the site
+</div>`;
+
+content.appendChild(heroSection);
+
+menuBtn.addEventListener("click", () => {
+  if (content.contains(heroSection)) {
+    content.removeChild(heroSection);
+    content.appendChild(menuSection);
+  }
+});
